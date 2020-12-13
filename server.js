2903441b159518535ed8a94d983930baf7340d92
Kala Foods <guhan.delta@gmail.com>
2020-12-13 16:24:18 -0500

    Initial Commit => Created a simple Express server

--- server.js
+++ server.js
@@ -1,46 +1,6 @@
-const { GraphQLServer } = require('graphql-yoga');
+const express = require('express');
 
-const messages = []; 
+const app = express();
+const PORT = 4000;
 
-// Types or Schema for GraphQL server => GraphQL Server Defn
-const typeDefs = `
-    type Message{
-        id: ID!
-        user: String!
-        content: String!
-    }
-
-    type Query{
-        messages: [Message!] 
-    }
-
-    type Mutation {
-        postMessage(user: String!, content: String!): ID!
-    }
-`
-// messages: [Message!]  => returns an array of messages
-// postMessage( container,{args} )
-
-// Resolvers help retrieve the data, as per the typeDefs, by matching the keys in the typeDef
-const resolvers = {
-    Query:{
-        messages: () => messages,
-    },
-    Mutation:{
-        postMessage: (parent, {user, content}) => {
-            const id = messages.length;
-            messages.push({
-                id, 
-                user,
-                content
-            });
-            return id;
-        },
-    },
-};
-
-const server = new GraphQLServer({ typeDefs, resolvers });
-
-server.start(({port})=>{
-    console.log(`Server running on port: ${port}`);
-})
+app.listen(PORT, () => console.log(`Server Listening on port: ${PORT}`));
