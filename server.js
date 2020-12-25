dd977ba3191c2af8c335f93aa0b9e2ce2a9fd060
Kala Foods <guhan.delta@gmail.com>
2020-12-25 17:12:33 -0500

    Registered the Express Server with GraphQL

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
 
 const app = express();
 const PORT = 4000;
 
-app.listen(PORT, () => console.log(`Server Listening on port: ${PORT}`));
+app.use('/graphql', graphqlHTTP({
+    graphiql: true // graphiql is a development tool that allows to query against the server, only to be used-
+   //- in dev env
+}));
+
+app.listen(PORT, () => {
+    console.log(`Server Listening on port: ${PORT}`)
+});
+ 
