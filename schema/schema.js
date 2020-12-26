6c00e2b1726ff24404754a95bfa076820ad21024
Kala Foods <guhan.delta@gmail.com>
2020-12-25 20:31:17 -0500

    Created the RootQuery for the GrapQL DB

--- schema/schema.js
+++ schema/schema.js
@@ -11,10 +11,35 @@ const {
 
 // GraphQLObjectType instructs GraphQL that how an user obj looks like 
 const UserType = new GraphQLObjectType({
-    name: 'User', 
+    name: 'User', // Obj name, usually starts with an upperCase
     fields: { // Most important prop here, which tells GraphQL about all the diff props that a user/userObj has
-        id: {type: GraphQLString },
-        firstName: {type: GraphQLString },
-        age: {type: GraphQLInt }
+        id: { type: GraphQLString },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt }
+    }
+});
+
+// RootQuery -> represents all the entrypoints of the GraphQL API | It's quite like teh entry point for App Data
+// RootQuery allows GraphQL to jump and land on a specidif node, in the graph of the entire data
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        // This tells GraphQL as any request for a user, corresponds to the UserType(defined ⇧)
+        // Instructing GraphQL as -> user is an obj of type UserType and has args id of type GraphQLString
+        user:{
+            // The below defn means, if any request for a user provides a userId, the RootQuery will return a-
+            //- UserType  
+            type: UserType,
+            args: {
+                    id:{
+                        type: GraphQLString
+                }
+            },
+            // resolve() is very **, as it is what that dives into the db to fetch the data
+            resolve(parentValue, args){ //args represents the arguments that were passed into the query, which-
+               //- in this case is id | args will have the args passed into the original query => id in this case
+                
+            }
+        }
     }
 });
